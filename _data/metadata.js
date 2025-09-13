0fbae4b88bf16f0fb96aba59736af2ea180ff0bc
jeff sites <Jeff.sites@gmail.com>
2025-09-12 19:12:33 -0700

    Update metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,6 +1,6 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "Jeff's Blog",
+	url: "https://jeffsites.github.io/jeffsites-blog/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
